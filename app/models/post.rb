1226cf2bb8b063e6e6ac4f82cdfbf628936f4478
ehiaghe aisiri <ehiaghe.aisiri@gmail.com>
2024-01-08 22:32:37 +0100

    posts table created

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,3 @@
 class Post < ApplicationRecord
-  belongs_to :user
-  belongs_to :category
+  validates :title, :content, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_04_131858) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_08_212641) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,6 +63,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_04_131858) do
     t.string "overview"
     t.string "poster_url"
     t.float "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
