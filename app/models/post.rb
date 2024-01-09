766ccec14fd1832050f4e45a40d386c633dec518
ehiaghe aisiri <ehiaghe.aisiri@gmail.com>
2024-01-09 15:01:20 +0100

    added validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, length: {minimium: 5, maximium: 50}
-  validates :content, presence: true, length: {minimium: 50, maximium: 500}
+  validates :title, presence: true, length: {minimum: 5, maximum: 50}
+  validates :content, presence: true, length: {minimum: 50, maximum: 500}
 end
